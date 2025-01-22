201a36e1572875a090c280994718ac7c7afd9123
DipaliPanchivishe <89767015+DipaliPanchivishe@users.noreply.github.com>
2025-01-22 18:25:18 +0000

    dataTypes and null obj

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+//tc39.es ecma   STANDARDS OF JS it will show on ECMA
+
+//PRIMITIVE DATATYPES
+/*
+boolean = true/false
+string = ""
+bigint
+null = standalone value
+ex: you want to give some temp but that is not visible due to tech issue so  that time we can use 
+undifined   =EX: var accID; without value 
+symbol= unique
+*/
+
+
+//Object
+let A= 10;
+
+console.log(typeof A);    //number is a type
+console.log(typeof null)  //Object    (null is a obj in JS)
+console.log(typeof undefined)  //undefined is a type 
+
+
