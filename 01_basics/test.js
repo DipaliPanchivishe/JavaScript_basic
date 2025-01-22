e33bd279adfe9f45b18325df3af5daa50e309291
DipaliPanchivishe <89767015+DipaliPanchivishe@users.noreply.github.com>
2025-01-22 18:05:53 +0000

    container config

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,20 @@
-console.log("Dipali")
+const A=12345;
+console.log(A);
+
+var accountIDs = "23";   /* dont use var in JS beacuase of issue in block and functions scope */
+var AccName = "ICAI";
+console.log(AccName);
+console.log(accountIDs)
+
+let B="raj";
+B=1;
+console.log(B);
+
+let AccountState;   //undifined 
+console.log(AccountState);
+
+
+AccCity = "Pune"    
+console.log(AccCity); 
+
+
