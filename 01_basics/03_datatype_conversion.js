5ef0f15aeb32009abbd6050831d1cc798654c8a8
DipaliPanchivishe <89767015+DipaliPanchivishe@users.noreply.github.com>
2025-01-22 18:58:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,19 @@
+let A=33;
+console.log(typeof A);  //number
+
+let B = "33"
+console.log(typeof B);  //string
+
+let valueinNo=Number(B)  // converting string to no 
+console.log(typeof valueinNo)   //B converted from string to number 
+
+A="33AB"
+let stringIntoNumber=Number(A)
+console.log(typeof stringIntoNumber)   //it will print type as number 
+console.log(stringIntoNumber)           //value is NAN means 22AB is not a number 
+
+
+
+//true = 1;  false = 2
+
+// false "";  true="fuop"
