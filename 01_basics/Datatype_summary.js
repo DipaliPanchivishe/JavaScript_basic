51f4959b0710f47c00626771a0a2a7be6844b507
DipaliPanchivishe <89767015+DipaliPanchivishe@users.noreply.github.com>
2025-01-23 09:34:27 +0000

    primitives datatypes

---
+++ 01_basics/Datatype_summary.js
@@ -0,0 +1,21 @@
+// primitive and non-primitive 
+//primitive (call by value)
+// 7 string, number,boolean, null(not empty not 0 ),undifined, symbol(it will help to make unique),bigint
+
+const score = 100
+const isLoggedIn = true
+const outsideTemp = null
+let useremail;
+
+let id = Symbol('12')
+let id1 = Symbol('12')
+console.log(id==id1)  //false 
+
+
+// non-primitive(call by reference)
+// array, obj, function
+
+
+
+
+//JS is dynamicly typed language
